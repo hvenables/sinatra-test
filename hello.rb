0a91ce8db9aadc16dafc20f63da30fcafe3c3dfa
Harry Venables <venablesh@yahoo.co.uk>
2015-09-14 15:46:40 +0100

    Added HTML and CSS

--- hello.rb
+++ hello.rb
@@ -1,9 +1,15 @@
 require 'sinatra'
 
 get '/' do
-  'hello!'
+  "<div style='border: 3px dashed red'>
+     <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
 
 get '/secret' do
   'This is a secret page'
+end
+
+get '/WHAT' do
+  'WHAT!!!'
 end
